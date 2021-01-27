cc703b8f80395c07ff491bbd8fc3d835cf0e7d25
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-01-26 23:59:09 -0300

    Mapping keys and add movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -11,6 +13,8 @@ struct Tcod {
 }
 
 fn main() {
+    tcod::system::set_fps(LIMIT_FPS);
+
     let root: Root = Root::initializer()
         .font("static/arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
@@ -19,13 +23,35 @@ fn main() {
         .init();
 
     let mut tcod = Tcod { root };
-    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
 
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit { break; }
     }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, ..} => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        },
+        Key { code: Escape, .. } => return true
+
+        _ => {}
+    }
+    false
 }
