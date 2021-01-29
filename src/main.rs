890de9cfa412427358ba93384ad2c6ca0d5c0abc
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-01-29 00:15:59 -0300

    Adding funcions to create tunnels

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::cmp;
+
 use tcod::colors::*;
 use tcod::console::*;
+
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
 
@@ -189,5 +192,17 @@ fn create_room(room: Rect, map: &mut Map) {
         for y in (room.y1 + 1)..room.y2 {
             map[x as usize][y as usize] = Tile::empty();
         }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2:i32, x: i32, map:&mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty()
     }
 }
