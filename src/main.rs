57a5b3b4fc4a347f02d37706120e582e07b6745d
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-01-28 23:45:34 -0300

    Adding two pillars to test map

--- src/main.rs
+++ src/main.rs
@@ -136,6 +136,9 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
 fn make_map() -> Map {
     let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
     map
 }
 
