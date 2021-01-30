92b2c5e6b059e89785d459a26f166eeba1a616cb
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-01-30 12:11:56 -0300

    Building map walls

--- src/main.rs
+++ src/main.rs
@@ -147,13 +147,14 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
 }
 
 fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
     let room1 = Rect::new(20, 15, 10, 15);
     let room2 = Rect::new(50, 15, 10, 15);
 
     create_room(room1, &mut map);
     create_room(room2, &mut map);
+    create_h_tunnel(25,55,23, &mut map);
 
     map
 }
