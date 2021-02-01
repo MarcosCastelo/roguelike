f7e3a1de3a0abbfc6168e6d31b11860832805334
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-01-31 23:24:33 -0300

    Adding verification after add some object

--- src/main.rs
+++ src/main.rs
@@ -332,20 +332,22 @@ fn create_v_tunnel(y1: i32, y2:i32, x: i32, map:&mut Map) {
     }
 }
 
-fn place_objects(room: Rect, objects: &mut Vec<Object>) {
+fn place_objects(room: Rect, map: &Map, objects: &mut Vec<Object>) {
     let num_monsters = rand::thread_rng().gen_range(0, MAX_ROOM_MONSTERS + 1);
     
     for _ in 0..num_monsters {
         let x = rand::thread_rng().gen_range(room.x1 + 1, room.x2);
         let y = rand::thread_rng().gen_range(room.y1 + 1, room.y2);
+        
+        if !is_blocked(x, y, map, Objects) {
+            let mut monster = if rand::random::<f32>() < 0.8 {
+                Object::new(x, y, 'o', DESATURATED_GREEN)
+            } else {
+                Object::new(x, y, 'T', DARKER_GREEN)
+            };
 
-        let mut monster = if rand::random::<f32>() < 0.8 {
-            Object::new(x, y, 'o', DESATURATED_GREEN)
-        } else {
-            Object::new(x, y, 'T', DARKER_GREEN)
-        };
-
-        objects.push(monster);
+            objects.push(monster);
+        }
     }
 }
 
